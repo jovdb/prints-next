8129d733ca7cc22504026d1489e320820c41321e
Jo Van den Berghe <jo.vandenberghe@smartphoto.com>
2022-10-09 10:28:02 +0200

    Inline smartphoto svg logo in header

--- components/logo/logo.tsx
+++ components/logo/logo.tsx
@@ -1,8 +1,12 @@
 import styles from "./logo.module.css";
+import SmartphotoLogo from "../../public/logo.svg";
 
-export const Logo = () => (
-	<div className={styles.logo}>
-		<img src={"./logo.svg"} alt="Logo"/>
-	</div>
-);
+export const Logo = () => {
+	return (
+		<div className={styles.logo}>
+			{/* To investigate; viewBox property is lost from svg */}
+			<SmartphotoLogo width="671" height="153" viewBox="0 0 671 153"/>
+		</div>
+	);
+};
 Logo.displayName = "Logo";
