eaee94cd2d912457d8dba2616777343416133f41
jovdb <jo.van.den.berghe@gmail.com>
2022-10-10 20:14:00 +0000

    Deploying to gh-pages from @ jovdb/prints-next@ec4d3419c3d8ba56a4ff694f1cf8f011dd6185c7 🚀

---
+++ _next/static/_nRQ436pUvsnJABid0XtA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],"/[locale]":["static/css/9f6836ea61327049.css","static/chunks/pages/[locale]-15849906871e843c.js"],sortedPages:["/_app","/_error","/[locale]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
