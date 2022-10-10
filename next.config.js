1b237800091c7b5e8db16b332cc46a251d7afc0d
Van den Berghe Jo <jo.van.den.berghe@gmail.com>
2022-10-10 22:45:09 +0200

    Update next.config.js to set path for github pages deploy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -10,6 +13,20 @@ const nextConfig = {
     })
 
     return config
+  },
+
+  /**
+   * Tell Next.js where the `public` folder is.
+   * Replace `prints-next` with your Github repo project name.
+   */
+  assetPrefix: isProd ? "/prints-next/" : "",
+
+  /**
+   * Disable server-based image optimization.
+   * @see https://nextjs.org/blog/next-12-3#disable-image-optimization-stable
+   */
+   images: {
+    unoptimized: true,
   },
 }
 
